7e56bcca9d7866f8435065087e39e2d304f0f97e
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-15 14:06:09 +0600

    book and see dettails page add

--- src/pages/services.js
+++ src/pages/services.js
@@ -71,6 +71,11 @@ export default function Services() {
 
   const products = searchTerm ? searchResults : filteredProducts;
 
+
+
+ console.log(products)
+
+
   return (
     <Layout>
       <nav aria-label="Breadcrumb" className="flex mt-4 max-w-6xl mx-auto">
@@ -188,117 +193,49 @@ export default function Services() {
               <div className="grid gap-3 grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
                 {products &&
                   products?.data.map((item) => (
-                    <div className="w-full px-3 mb-6 ">
-                      <div className="border border-gray-200 rounded-md dark:border-gray-800">
-                        <div className="relative bg-gray-200">
-                          <a href="#" className="">
-                            <img
-                              src={item?.thumbnail}
-                              alt=""
-                              className="object-cover w-full h-56 mx-auto "
-                            />
-                          </a>
-                        </div>
-                        <div className="p-5 bg-gray-50 dark:bg-gray-900">
-                          <div className="flex items-center justify-between mb-4">
-                            <h3 className="text-xl font-medium dark:text-gray-400">
-                              {item?.title}
-                            </h3>
-                            <a href="#" className="">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                className="text-red-500 dark:text-gray-400 bi bi-heart"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
-                              </svg>
-                            </a>
-                          </div>
-                          <div className="mb-4 ">
-                            <p className="text-lg ">
-                              <span className="text-red-400 dark:text-gray-400">
-                                $700.00
-                              </span>
-                              <span className="ml-2 text-gray-400 line-through dark:text-gray-400">
-                                1500
-                              </span>
-                            </p>
-                          </div>
-                          <div className="flex items-center justify-between mb-4">
-                            <ul className="flex ">
-                              <li>
-                                <a href="#">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="w-4 mr-1 text-yellow-500 dark:text-gray-400 bi bi-star "
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                  </svg>
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="w-4 mr-1 text-yellow-500 dark:text-gray-400 bi bi-star"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                  </svg>
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="w-4 mr-1 text-yellow-500 dark:text-gray-400 bi bi-star"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                  </svg>
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="w-4 mr-1 text-yellow-500 dark:text-gray-400 bi bi-star"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
-                                  </svg>
-                                </a>
-                              </li>
-                            </ul>
-                            <p className="text-base font-normal text-gray-400 dark:text-gray-400">
-                              <span>Nepal</span>
-                            </p>
-                          </div>
-                          <a
-                            href="#"
-                            className="flex justify-center px-4 py-2 text-blue-600 border border-blue-300 rounded-full dark:border-gray-600 dark:text-gray-400 hover:bg-blue-700 hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900"
-                          >
-                            Add to cart
-                          </a>
+                    <div className="border border-gray-200 rounded-md dark:border-none">
+                    <div>
+                      <a href="#" className="">
+                        <img
+                          src={item?.thumbnail}
+                          alt=""
+                          className="object-cover w-full h-56 mx-auto "
+                        />
+                      </a>
+                    </div>
+                    <div className="p-6 bg-gray-50 dark:bg-gray-900">
+                      <h3 className="mb-2 text-xl font-medium dark:text-gray-400">
+                        {item?.title}
+                      </h3>
+                      <div className="mb-2 text-[#10569f] dark:text-[#10569f]">
+                        {item?.additionalDetails.map((info) => info)}
+                      </div>
+                      <div className="mb-2 flex-flex-row text-[#10569f] ">
+                        <div> Availability</div>
+                        <div>
+                          <div>{item?.availability?.hours}</div>
                         </div>
                       </div>
+                      <div className="flex justify-between mb-2">
+                        <p className="mb-2 text-lg font-bold text-gray-600 dark:text-gray-400 ">
+                          <span>BasePrice:  ${item?.pricing?.basePrice}</span>
+                        </p>
+                     
+                      </div>
+                      <a
+                        href="#"
+                        className="flex justify-center px-4 py-2 text-[#10569f] border border-[#10569f] rounded-full dark:border-gray-600 dark:text-gray-400 hover:bg-[#10569f] hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900"
+                      >
+                        Book Now 
+                      </a>
+                      <a
+                        href="#"
+                        className="flex justify-center px-4 py-2 my-2 text-[#10569f] border border-[#10569f] rounded-full dark:border-gray-600 dark:text-gray-400 hover:bg-[#10569f] hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900"
+                      >
+                      See Dettails 
+                      </a>
                     </div>
+                  </div>
                   ))}
               </div>
 
